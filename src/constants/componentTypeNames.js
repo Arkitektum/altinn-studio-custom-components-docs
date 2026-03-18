a162ee21d3de67d446d1f66385989213b5ae3076
Benjamin Dehli <superelg@gmail.com>
2026-03-18 16:20:22 +0100

    Add constants for component names and types
    Define Norwegian display names for various custom components and their categories.

---
+++ src/constants/componentTypeNames.js
@@ -0,0 +1,11 @@
+export default {
+    typography: "Typografi",
+    field: "Felt",
+    list: "Liste",
+    descriptionList: "Beskrivelsesliste",
+    table: "Tabell",
+    summation: "Summering",
+    group: "Gruppe",
+    grouplist: "Gruppeliste",
+    layout: "Underskjema"
+};

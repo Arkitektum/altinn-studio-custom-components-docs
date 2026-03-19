43154fc9aa7332f9d5fe9738ed850de75a39c1e9
Benjamin Dehli <superelg@gmail.com>
2026-03-19 08:58:41 +0100

    Add localized text resources for UI components
    Define Norwegian (nb) labels and headers for custom fields, lists, tables, and summation components. These resources provide the display text for various layout elements and data model fields.

---
+++ src/data/textResources.js
@@ -0,0 +1,145 @@
+export default {
+    language: "nb",
+    resources: [
+        {
+            id: "resource.customField.data.title",
+            value: "Label for datafelt"
+        },
+        {
+            id: "resource.customField.booleanData.title",
+            value: "Label for boolean-data felt"
+        },
+        {
+            id: "resource.customField.booleanText.title",
+            value: "Label for boolean-text felt"
+        },
+        {
+            id: "resource.customField.countData.title",
+            value: "Label for count-data felt"
+        },
+        {
+            id: "resource.customField.kode.title",
+            value: "Label for kodefelt"
+        },
+        {
+            id: "resource.customField.part.navn.title",
+            value: "Organisasjon"
+        },
+        {
+            id: "resource.utfallBesvarelse.utfallSvar.svv.header",
+            value: "Svar på vilkår for midlertidig brukstillatelse"
+        },
+        {
+            id: "resource.utfallBesvarelse.utfallSvar.mkv.header",
+            value: "Svar på oppfølgingspunkt for midlertidig brukstillatelse"
+        },
+        {
+            id: "resource.customHeader.text.title",
+            value: "Custom header text"
+        },
+        {
+            id: "resource.customParagraph.text.title",
+            value: "Custom paragraph text"
+        },
+        {
+            id: "resource.customSubheader.text.title",
+            value: "Custom subheader text"
+        },
+        {
+            id: "resource.customList.data.title",
+            value: "Elementer"
+        },
+        {
+            id: "resource.customList.vedlegg.title",
+            value: "Vedlegg"
+        },
+        {
+            id: "resource.rammebetingelser.arealdisponering.emptyFieldText",
+            value: "Ingen planer registrert"
+        },
+        {
+            id: "resource.customSummation.data.title",
+            value: "Oppsummering av arealdata"
+        },
+        {
+            id: "resource.customSummation.areaBasis.title",
+            value: "Beregningsgrunnlag"
+        },
+        {
+            id: "resource.customSummation.areaMinus.title",
+            value: "Ev. areal som trekkes fra iht. beregningsregler"
+        },
+        {
+            id: "resource.customSummation.areaEquals.title",
+            value: "Areal som skal byggesesakbehandles"
+        },
+        {
+            id: "resource.arbeidsplasser.title",
+            value: "Hvilke arbeidsplasser er berørt av tiltaket"
+        },
+        {
+            id: "resource.arbeidsplasser.emptyFieldText",
+            value: "Ingen arbeidsplasser registrert"
+        },
+        {
+            id: "resource.arbeidsplasser.arbeidsplasserKey.title",
+            value: "Arbeidsplasser"
+        },
+        {
+            id: "resource.arbeidsplasser.beroertAvTiltaket.title",
+            value: "Berørt av tiltaket"
+        },
+        {
+            id: "resource.arbeidsplasser.eksisterende.title",
+            value: "Eksisterende arbeidsplasser"
+        },
+        {
+            id: "resource.arbeidsplasser.faste.title",
+            value: "Faste arbeidsplasser"
+        },
+        {
+            id: "resource.arbeidsplasser.framtidige.title",
+            value: "Framtidige arbeidsplasser"
+        },
+        {
+            id: "resource.arbeidsplasser.midlertidige.title",
+            value: "Midlertidige arbeidsplasser"
+        },
+        {
+            id: "resource.arbeidsplasser.utleieBygg.title",
+            value: "Utleiebygg"
+        },
+        {
+            id: "resource.customTable.data.title",
+            value: "Data"
+        },
+        {
+            id: "resource.customTable.data.emptyFieldText",
+            value: "Ingen data registrert"
+        },
+        {
+            id: "resource.customTable.data.dato.title",
+            value: "Dato"
+        },
+        {
+            id: "resource.customTable.data.kommentar.title",
+            value: "Kommentar"
+        },
+        {
+            id: "resource.naboGjenboer.eiendommer.eiendom.matrikkelinformasjon.emptyFieldText",
+            value: "Ingen eiendommer registrert"
+        },
+        {
+            id: "resource.customGrouplist.ettersending.title",
+            value: "Ettersendinger"
+        },
+        {
+            id: "resource.customGrouplist.ettersending.emptyFieldText.default",
+            value: "Ingen ettersendinger registrert"
+        },
+        {
+            id: "resource.customDescriptionList.data.title",
+            value: "Arrangementer"
+        }
+    ]
+};

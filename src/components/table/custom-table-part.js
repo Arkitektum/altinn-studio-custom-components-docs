5d6aa44bba81f2bc5491e5e02677370de8af1599
Benjamin Dehli <superelg@gmail.com>
2026-05-05 11:00:06 +0200

    Simplify 'tiltakshaver' title resource key path
    Removes the 'kontaktperson' segment from the title resource key in custom-table-part, standardizing it to the more general 'resource.tiltakshaver.title'. This also removes the now unused 'partType' definitions.

--- src/components/table/custom-table-part.js
+++ src/components/table/custom-table-part.js
@@ -3,14 +3,11 @@ const markup = {
     type: "Custom",
     tagName: "custom-table-part",
     size: "h2",
-    partType: "tiltakshaver.kontaktperson",
     hideIfEmpty: true,
     dataModelBindings: {
         data: "tiltakshaver.kontaktperson"
     }
 };
-
-const partType = "tiltakshaver.kontaktperson";
 
 const defaultResourceBindings = {
     navn: {
@@ -25,7 +22,7 @@ const defaultResourceBindings = {
         title: `resource.part.epost.title`,
         emptyFieldText: "resource.emptyFieldText.default"
     },
-    title: `resource.${partType}.title`,
+    title: `resource.tiltakshaver.title`,
     emptyFieldText: "resource.emptyFieldText.default"
 };
 export default { markup, defaultResourceBindings };
